6a7162e72390b7119763ace7b8028ca51e760b0c
nisshii0313 <yuta19990313@gmail.com>
2019-09-11 03:05:36 +0900

    fix lint warning

--- pages/skills.vue
+++ pages/skills.vue
@@ -2,7 +2,7 @@
   <span v-touch:swipe.left="swipeHandler_left" v-touch:swipe.right="swipeHandler_right">
     <div class="Chart">
       <h2 style="text-align:center;">Skills</h2>
-      <radar/>
+      <radar />
     </div>
   </span>
 </template>
@@ -15,6 +15,7 @@ import Radar from '../components/Radar'
 Vue.use(Vue2TouchEvents)
 
 export default {
+  components: { Radar },
   methods: {
     swipeHandler_left() {
       location.href = '../history'
@@ -22,7 +23,6 @@ export default {
     swipeHandler_right() {
       location.href = '../inspire'
     }
-  },
-  components: { Radar }
+  }
 }
 </script>
