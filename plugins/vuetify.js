0729efe6e3ad7ed019245c15de302e24d9b973f1
nisshii0313 <yuta19990313@gmail.com>
2019-10-11 16:42:32 +0900

    additional fix

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import colors from 'vuetify/es5/util/colors'
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
 import { faTwitter, faFacebook, faGithub, faBloggerB } from '@fortawesome/free-brands-svg-icons'
 
@@ -11,13 +10,9 @@ dom.i2svg()
 
 export default new Vuetify({
   theme: {
-    new_dark_theme: '#000f1b',
-    primary: colors.blue.darken2,
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    new_dark_theme: '#000f1b'
+  },
+  icons: {
+    iconfont: 'fa'
   }
 })
